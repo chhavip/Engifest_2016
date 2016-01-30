2f15fb66a356d7fcba57ae0c5207a8af2c12dbd2
shikhark211 <shikhark211@gmail.com>
2016-01-30 05:57:33 +0530

    added explore button background and accommodation on home

--- app/src/main/java/com/dtu/engifest/HomeFragment.java
+++ app/src/main/java/com/dtu/engifest/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.dtu.engifest;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -10,6 +12,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +31,8 @@ import java.util.HashMap;
  */
 public class HomeFragment extends Fragment {
     protected   SliderLayout sliderShow;
+    CardView explore;
+    CardView accommodation;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +44,8 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.home_fragment_layout, container, false);
 
         sliderShow = (SliderLayout) view.findViewById(R.id.slider);
-
+        explore = (CardView) view.findViewById(R.id.explore);
+        accommodation = (CardView) view.findViewById(R.id.accommodation);
         /*HTextView about_engifest = (HTextView)view.findViewById(R.id.about_engifest);
 
         String aboutEngi = "Thrill, joy and ecstasy are words that are synonymous with ENGIFEST";
@@ -66,7 +72,23 @@ public class HomeFragment extends Fragment {
             sliderShow.setPresetTransformer(SliderLayout.Transformer.Accordion);
             sliderShow.setPresetIndicator(SliderLayout.PresetIndicators.Right_Bottom);
 
-        return view;
+        accommodation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://docs.google.com/forms/d/1MajWbBidj5w2RnO1kX6MynJ5ANzkHD2RFcD0AreQ5H0/viewform?c=0&w=1";
+                try {
+                    Intent i = new Intent("android.intent.action.MAIN");
+                    i.setComponent(ComponentName.unflattenFromString("com.android.chrome/com.android.chrome.Main"));
+                    i.addCategory("android.intent.category.LAUNCHER");
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                } catch (ActivityNotFoundException e) {
+                    // Chrome is not installed
+                    Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(i);
+                }
+            }
+        });        return view;
     }
 
     @Override
